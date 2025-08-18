2599ad69e1daf8a0f5f73dc1b90d787d3eac091a
bawfeh <bawfeh@gmail.com>
2025-08-18 08:04:43 -0400

    added contact_us.html, adjust css file

---
+++ travel_recommendation.js
@@ -0,0 +1,4 @@
+const addPatientButton = document.getElementById("addBooking");
+const report = document.getElementById("report");
+const btnSearch = document.getElementById('btnSearch');
+const patients = [];
